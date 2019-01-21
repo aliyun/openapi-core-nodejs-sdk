48e0c83d1f46ecc1f5d6c8bbcdefdfa9d716cf7f
朴灵 <puling.tyq@alibaba-inc.com>
2019-01-21 23:59:29 +0800

    split unittest/integration test cases

--- test/roa.test.js
+++ test/roa.test.js
@@ -106,39 +106,4 @@ describe('roa core', function() {
       });
     });
   });
-
-  describe('request', function () {
-    var client = new ROAClient({
-      accessKeyId: process.env.ACCESS_KEY_ID,
-      accessKeySecret: process.env.ACCESS_KEY_SECRET,
-      endpoint: 'http://ros.aliyuncs.com',
-      apiVersion: '2015-09-01'
-    });
-
-    it('request', async function () {
-      this.timeout(10000);
-      var result = await client.request('GET', '/regions', {}, '', {}, {
-        timeout: 10000
-      });
-      expect(result).to.have.property('Regions');
-    });
-
-    it('get should ok', async function () {
-      this.timeout(10000);
-      var result = await client.get('/regions', {}, {}, {
-        timeout: 10000
-      });
-      expect(result).to.have.property('Regions');
-    });
-
-    it('get raw body should ok', async function () {
-      this.timeout(10000);
-      var opts = {
-        rawBody: true,
-        timeout: 10000
-      };
-      var result = await client.get('/regions', {}, {}, opts);
-      expect(result).to.be.a('string');
-    });
-  });
 });
--- test/rpc.test.js
+++ test/rpc.test.js
@@ -172,50 +172,5 @@ describe('rpc core', function() {
         ['c', 'value']
       ])).to.be('a=value&b=value&c=value');
     });
-
-  });
-
-  describe('request', function() {
-    var client = new RPCClient({
-      accessKeyId: process.env.ACCESS_KEY_ID,
-      accessKeySecret: process.env.ACCESS_KEY_SECRET,
-      endpoint: 'https://ecs.aliyuncs.com',
-      apiVersion: '2014-05-26'
-    });
-
-    it('should ok', async function() {
-      this.timeout(10000);
-
-      var params = {
-        key: ['1', '2', '3', '4', '5', '6', '7', '8', '9',
-          '10', '11']
-      };
-
-      var requestOption = {
-        method: 'POST',
-        timeout: 10000
-      };
-
-      const result = await client.request('DescribeRegions', params, requestOption);
-      expect(result).to.have.key('RequestId');
-      expect(result).to.have.key('Regions');
-    });
-
-    it('should ok with repeat list less 10 item', async function() {
-      this.timeout(10000);
-
-      var params = {
-        key: ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-      };
-
-      var requestOption = {
-        method: 'POST',
-        timeout: 10000
-      };
-
-      const result = await client.request('DescribeRegions', params, requestOption);
-      expect(result).to.have.key('RequestId');
-      expect(result).to.have.key('Regions');
-    });
   });
 });
