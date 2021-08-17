e886aa28424c11080e21cc5c53455419500b58c3
Jackson Tian <shyvo1987@gmail.com>
2021-08-17 10:56:40 +0800

    Add securityToken into definition file

--- lib/rpc.d.ts
+++ lib/rpc.d.ts
@@ -38,6 +38,7 @@ declare namespace RPCClient {
         apiVersion: string;
         accessKeyId: string;
         accessKeySecret: string;
+        securityToken?: string;
         codes?: (string | number)[];
         opts?: object;
     }
